835cb50d8646434fd8b6448a1fb456355e346bbb
Luis Conejo <luisconej@gmail.com>
2024-02-21 18:28:47 -0800

    Commiting failing code for instructors to take a look.

--- sample_code.py
+++ sample_code.py
@@ -2,6 +2,7 @@ def say_hello(user_name):
     return f"Hi, there {user_name}!"
 
 if __name__ == "__main__":
+    input = "Hi there!"
     user_name = input("What is your name? ")
     response = say_hello(user_name)
     print(response)
